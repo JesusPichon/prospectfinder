90282c9b6e43215f24cb9c0162fc0eea280e00e5
EnriqueRV <sergioenri2001@gmail.com>
2025-05-17 14:35:33 -0600

    feat: Implementado DashboardLayout y se actualizó la estructura de routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,16 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import App from "./App.tsx";
 import Home from "./pages/Home.tsx";
+import DashboardLayout from "./layouts/DashboardLayout.tsx";
 import "./styles.css";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DashboardLayout />,
+    children: [{ index: true, element: <Home /> }],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
